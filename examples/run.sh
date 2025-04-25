d037238fcb9cb8981573a4b1d9258dd2c5ca6e0a
Usama Ahmed Siddiquie <220usamaahmed@gmail.com>
2025-04-25 13:26:01 +0200

    add run script

---
+++ examples/run.sh
@@ -0,0 +1,5 @@
+# python3 train_from_custom_dict.py --env_id 'SafetyAntVelocity-v1' --seed=0
+python3 train_from_custom_dict.py --env_id 'SafetySwimmerVelocity-v1' --seed=0
+python3 train_from_custom_dict.py --env_id 'SafetyHumanoidVelocity-v1' --seed=0
+python3 train_from_custom_dict.py --env_id 'SafetyWalker2dVelocity-v1' --seed=0
+python3 train_from_custom_dict.py --env_id 'SafetyHalfCheetahVelocity-v1' --seed=0
